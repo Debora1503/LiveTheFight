f3f6485dd6f91759a0196305078cf0e246b616e9
Debora <deborabandeirinha@gmail.com>
2024-06-15 02:03:07 +0100

    Adição de destaques a pagina casa

--- screens/casa.js
+++ screens/casa.js
@@ -1,9 +1,8 @@
 import React, { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Dimensions, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Carousel from 'react-native-snap-carousel';
-import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 import PropTypes from 'prop-types';
 
 const { width: viewportWidth } = Dimensions.get('window');
@@ -39,30 +38,56 @@ const Casa = ({ navigation }) => {
       >
         <Ionicons name="menu" size={32} color="white" />
       </TouchableOpacity>
-      <Text style={styles.header}>Welcome</Text>
-      <Carousel
-        ref={carouselRef}
-        data={entries}
-        renderItem={renderItem}
-        sliderWidth={viewportWidth}
-        itemWidth={viewportWidth * 0.8}
-        layout={'default'}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.buttonText}>Home</Text>
-      </TouchableOpacity>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Text style={styles.header}>Welcome</Text>
+        <Carousel
+          ref={carouselRef}
+          data={entries}
+          renderItem={renderItem}
+          sliderWidth={viewportWidth}
+          itemWidth={viewportWidth * 0.8}
+          layout={'default'}
+        />
+
+        <View style={styles.noticia}>
+          <Text style={styles.titulonuti}>Noticias:</Text>
+          <Text style={styles.tituloum}>Ultimos Campeonatos:</Text>
+          <Text style={styles.destaque}>Campeonato Europeu </Text>
+          <Text>Decorrino na Suécia de 2-6 de Maio Portugal conquistou 14 medalhas entre elas 1 primeiro lugar em Sanda Light</Text>
+          <View style={styles.imageContainer}>
+            <Image source={require('../img/GreciaSelecao.jpg')} style={styles.GreciaSelecao} />
+          </View>
+          <Text style={styles.destaque}>Campeonato Europeu </Text>
+          <Text>Decorrino na Suécia de 2-6 de Maio Portugal conquistou 14 medalhas entre elas 1 primeiro lugar em Sanda Light</Text>
+          <View style={styles.imageContainer}>
+            <Image source={require('../img/GreciaSelecao.jpg')} style={styles.GreciaSelecao} />
+          </View>
+          <Text style={styles.destaque}>Campeonato Europeu </Text>
+          <Text>Decorrino na Suécia de 2-6 de Maio Portugal conquistou 14 medalhas entre elas 1 primeiro lugar em Sanda Light</Text>
+          <View style={styles.imageContainer}>
+            <Image source={require('../img/GreciaSelecao.jpg')} style={styles.GreciaSelecao} />
+          </View>
+        </View>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
 
 Casa.propTypes = {
-    navigation: PropTypes.object.isRequired, // Adicione isso se necessário
-  };
+  navigation: PropTypes.object.isRequired,
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'gray',
+  },
+  scrollContainer: {
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 60,
@@ -71,6 +96,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 40,
     left: 20,
+    zIndex: 1,
   },
   header: {
     fontSize: 24,
@@ -100,6 +126,46 @@ const styles = StyleSheet.create({
     color: 'white',
     marginTop: 20,
     fontSize: 18,
+  },
+  noticia: {
+    backgroundColor: '#A8A8A8',
+    marginTop: 10,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  titulonuti: {
+    fontSize: 20,
+  },
+  tituloum: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  destaque: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  imageContainer: {
+    marginTop: 10,
+    width: '100%',
+    alignItems: 'center',
+  },
+  GreciaSelecao: {
+    width: 170,
+    height: 170,
+    resizeMode: 'cover',
+    marginTop: 10,
+    marginBottom:15,
+    borderRadius:10,
+    shadowColor: "black",
+    shadowOffset: {
+        width: 0,
+        height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
   },
 });
 
